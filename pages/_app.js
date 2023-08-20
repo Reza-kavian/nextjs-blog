85fb47ce8dda9718fba49a4d6ed604dc754e800b
reza kavian <reza.kavian1986@gmail.com>
2023-08-20 16:36:13 +0330

    Upload simple nextjs project on Github

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+// import '../styles/global.css';//in file ba name global.css ra age dar _app.js import konim dar hame safakat emal mishe
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
